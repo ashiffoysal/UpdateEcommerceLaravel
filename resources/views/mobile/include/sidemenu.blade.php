f01b6090a268bbc9f08096376c9436e95cecafc4
durbarit3 <git3@durbarit.com>
2020-02-19 15:57:36 +0600

    hello world

---
+++ resources/views/mobile/include/sidemenu.blade.php
@@ -0,0 +1,285 @@
+<div class="panel-left__top clearfix text-center">
+				<div class="panel-logo">
+					<a href="index.html"><img src="{{asset('public/mobile')}}/image/demo-mobile/logo.png" title="Your Store" alt="Your Store"></a>
+				</div>
+				<div class="panel-search">
+					<div id="search" class="input-group">
+						<input type="text" name="search" value="" placeholder="Search" class="form-control input-lg">
+						<span class="input-group-btn">
+							<button type="button" class="btn btn-default btn-link"><i class="fa fa-search"></i></button>
+						  </span>
+					</div>
+				</div>
+			</div>
+			
+			<div class="panel-left__midde">
+				<div class="panel-group" id="panel-category" role="tablist" aria-multiselectable="true">
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab">
+							<a href="#">Jewelry</a>
+							<span class="head"><a class="pull-right accordion-toggle" data-toggle="collapse" data-parent="#panel-category" href="#panel-category1" aria-expanded="true"></a></span>
+						</div>
+						<div id="panel-category1" class="panel-collapse collapse " role="tabpanel">
+							<ul>
+								<li>
+									<a href="#">Necklaces</a>
+								</li>
+								<li>
+									<a href="#">Pearl Jewelry</a>
+								</li>
+								<li>
+									<a href="#">Slider 925</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab">
+							<a href="#">Womens</a>
+							<span class="head"><a class="pull-right accordion-toggle" data-toggle="collapse" data-parent="#panel-category" href="#panel-category2" aria-expanded="true"></a></span>
+						</div>
+						<div id="panel-category2" class="panel-collapse collapse " role="tabpanel">
+							<ul>
+								<li>
+									<a href="#">necklaces</a>
+								</li>
+								<li>
+									<a href="#">Pearl mens</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab">
+							<a href="#">Fashion</a>
+							<span class="head"><a class="pull-right accordion-toggle" data-toggle="collapse" data-parent="#panel-category" href="#panel-category3" aria-expanded="true"></a></span>
+						</div>
+
+						<div id="panel-category3" class="panel-collapse collapse " role="tabpanel">
+							<ul>
+								<li>
+									<a href="#">Gadgets &amp; Auto Parts</a>
+								</li>
+								<li>
+									<a href="#">More Car Accessories</a>
+								</li>
+								<li>
+									<a href="#">Alarms</a>
+								</li>
+								<li>
+									<a href="#">Printers</a>
+								</li>
+								<li>
+									<a href="#">Scanners</a>
+								</li>
+								<li>
+									<a href="#">Speakers</a>
+								</li>
+								<li>
+									<a href="#">Web Cameras</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab">
+							<a href="#">Shop Collection</a>
+							<span class="head"><a class="pull-right accordion-toggle" data-toggle="collapse" data-parent="#panel-category" href="#panel-category4" aria-expanded="true"></a></span>
+
+						</div>
+
+						<div id="panel-category4" class="panel-collapse collapse " role="tabpanel">
+							<ul>
+								<li>
+									<a href="#">Hanet magente</a>
+								</li>
+								<li>
+									<a href="#">Knage unget</a>
+								</li>
+								<li>
+									<a href="#">Latenge mange</a>
+								</li>
+								<li>
+									<a href="#">Punge nenune</a>
+								</li>
+								<li>
+									<a href="#">Qunge genga</a>
+								</li>
+								<li>
+									<a href="#">Tange manue</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab">
+							<a href="#">Electronics </a>
+							<span class="head"><a class="pull-right accordion-toggle" data-toggle="collapse" data-parent="#panel-category" href="#panel-category5" aria-expanded="true"></a></span>
+
+						</div>
+
+						<div id="panel-category5" class="panel-collapse collapse " role="tabpanel">
+							<ul>
+								<li>
+									<a href="#">Angene mafin</a>
+								</li>
+								<li>
+									<a href="#">Body Chains</a>
+								</li>
+								<li>
+									<a href="#">DIY Beads</a>
+								</li>
+								<li>
+									<a href="#">Egante mangetes</a>
+								</li>
+								<li>
+									<a href="#">Necklaces</a>
+								</li>
+								<li>
+									<a href="#">Rengae manges</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab">
+							<a href="#">Sports 2</a>
+							<span class="head"><a class="pull-right accordion-toggle" data-toggle="collapse" data-parent="#panel-category" href="#panel-category6" aria-expanded="true"></a></span>
+
+						</div>
+
+						<div id="panel-category6" class="panel-collapse collapse " role="tabpanel">
+							<ul>
+								<li>
+									<a href="#">Accessories</a>
+								</li>
+								<li>
+									<a href="#">Boys News</a>
+								</li>
+								<li>
+									<a href="#">Computers</a>
+								</li>
+								<li>
+									<a href="#">Electronics</a>
+								</li>
+								<li>
+									<a href="#">Fashion</a>
+								</li>
+								<li>
+									<a href="#">Girls New</a>
+								</li>
+								<li>
+									<a href="#">Men</a>
+								</li>
+								<li>
+									<a href="#">Mobiles</a>
+								</li>
+								<li>
+									<a href="#">Sports</a>
+								</li>
+								<li>
+									<a href="#">Women</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab">
+							<a href="#">Sports</a>
+							<span class="head"><a class="pull-right accordion-toggle" data-toggle="collapse" data-parent="#panel-category" href="#panel-category7" aria-expanded="true"></a></span>
+
+						</div>
+
+						<div id="panel-category7" class="panel-collapse collapse " role="tabpanel">
+							<ul>
+								<li>
+									<a href="#">Camping &amp; Hiking</a>
+								</li>
+								<li>
+									<a href="#">Cusen mariot</a>
+								</li>
+								<li>
+									<a href="#">Denta magela</a>
+								</li>
+								<li>
+									<a href="#">Engite nanet</a>
+								</li>
+								<li>
+									<a href="#">Theid cupensg</a>
+								</li>
+								<li>
+									<a href="#">Verture agoent</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+
+			<div class="panel-left__bottom clearfix text-center">
+				<div class="col-xs-6">
+					<i class="fa fa-check-square-o" aria-hidden="true"></i>
+					<div class="bot-inner">
+						<a href="compare.html">Compare</a>
+					</div>
+				</div>
+				<div class="col-xs-6">
+					<i class="fa fa-heart" aria-hidden="true"></i>
+					<div class="bot-inner">
+						<a href="wishlist.html">Wish List</a>
+					</div>
+				</div>
+
+				<div class="col-xs-6 panel-left__language">
+					<div class="btn-group languages-block">
+						<form action="#" method="post" enctype="multipart/form-data" id="form-language">
+							<div class="btn-group">
+								<button class="btn-link dropdown-toggle" data-toggle="dropdown">
+									<img src="{{asset('public/mobile')}}/image/demo/flags/gb.png" alt="English" title="English">
+									<span class="hidden-xs">English</span>
+									<i class="fa fa-angle-down"></i>
+								</button>
+
+								<ul class="dropdown-menu">
+									<li>
+										<button class="btn-block language-select" type="button" name="en-gb"><img src="{{asset('public/mobile')}}/image/demo/flags/gb.png" alt="English" title="English"> English</button>
+									</li>
+									<li>
+										<button class="btn-block language-select" type="button" name="ar"><img src="{{asset('public/mobile')}}/image/demo/flags/lb.png" alt="Arabic" title="Arabic"> Arabic</button>
+									</li>
+								</ul>
+							</div>
+							<input type="hidden" name="code" value="">
+							<input type="hidden" name="redirect" value="#">
+						</form>
+					</div>
+					<h4>Language</h4>
+				</div>
+				<div class="col-xs-6 panel-left__currency">
+					<div class="btn-group currencies-block">
+						<form action="#" method="post" enctype="multipart/form-data" id="form-currency">
+							<div class="btn-group">
+								<button class="btn-link dropdown-toggle" data-toggle="dropdown">
+									$ USD
+									<i class="fa fa-angle-down"></i>
+								</button>
+								<ul class="dropdown-menu">
+									<li>
+										<button class="currency-select btn-block" type="button" name="EUR">€ Euro</button>
+									</li>
+									<li>
+										<button class="currency-select btn-block" type="button" name="USD">$ USD</button>
+									</li>
+								</ul>
+							</div>
+							<input type="hidden" name="code" value="">
+							<input type="hidden" name="redirect" value="index.html">
+						</form>
+					</div>
+
+					<h4>Currency</h4>
+
+				</div>
+			</div>
