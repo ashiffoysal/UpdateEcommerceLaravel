f77885c228c388493b8dbf98548f965ccad2f62a
ashiffoysal-coder <ashiffoysal8818@gmail.com>
2020-02-26 18:08:02 +0600

    hdfhjdfhj

--- resources/views/mobile/ajax_blade/search/search_product_result.blade.php
+++ resources/views/mobile/ajax_blade/search/search_product_result.blade.php
@@ -2,11 +2,12 @@
 @php
     $productInFlashDeal = App\FlashDealDetail::where('product_id', $product->id)->where('status', 1)->first();
 @endphp
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <div class="product-layout col-xs-6">
     <div class="product-item-container">
         <div class="left-block">
             <div class="product-image-container ">
-                <a href="#" title="amper modi dolores">
+                <a href="{{ url('product/details/'.$product->slug.'/'.$product->id) }}" title="amper modi dolores">
                     <img src="{{ asset('public/uploads/products/thumbnail/mobile/'.$product->thumbnail_img) }}" title="{{ $product->product_name }}" class="img-responsive">
                 </a>
             </div>
@@ -22,7 +23,7 @@
 
         <div class="right-block">
             <div class="caption">
-                <h4><a href="{{ url('product/'.$product->slug.'/'.$product->id) }}">{{ $product->product_name }}</a></h4>
+                <h4><a href="{{ url('product/details/'.$product->slug.'/'.$product->id) }}">{{ $product->product_name }}</a></h4>
                 <div class="ratings">
                     <div class="rating-box">
                         <span class="fa fa-stack"><i class="fa fa-star-o fa-stack-1x"></i></span>
@@ -45,9 +46,9 @@
                         @php
                         $discountPrice = $product->product_price - $productInFlashDeal->discount;
                         @endphp
-                        @else
+                        @else($productInFlashDeal->discount_type == 2)
                          @php
-                        $discountPrice = $product->product_price /100 * $productInFlashDeal->discount;
+                        $discountPrice =$product->product_price - $product->product_price /100 * $productInFlashDeal->discount;
                         @endphp
                         @endif
                         ৳ {{ $discountPrice }}
@@ -63,11 +64,66 @@
                 </div>
                 <div class="button-group">
                     <button class="addToCart font-sn" type="button" title="Add to Cart" onclick="cart.add('175', '1');"> <i class="fa fa-shopping-cart"></i><span><span>Add to Cart</span></span></button>
-                    <button class="wishlist btn-button" type="button" title="Add to Wish List" onclick="wishlist.add('175');"><i class="fa fa-heart-o"></i></button>
-                    <button class="compare btn-button" type="button" title="Compare this Product" onclick="compare.add('175');"><i class="fa fa-retweet"></i></button>
+                    @if(Auth::guard('web')->check())
+                    <a class="wishlist btn-button mywishlist" id="mywishlist" data-id="{{$product->id}}"><i class="fa fa-heart-o"></i></a>
+                    @else
+                      <a href="{{url('/mobile/login')}}" class="wishlist btn-button"  title="Add to Wish List" href="" ><i class="fa fa-heart-o"></i></a>
+                    @endif
+                    <a class="compare btn-button compareproduct" type="button"  title="Compare this Product" id="compareproduct" data-id="{{$product->id}}"><i class="fa fa-retweet"></i></a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script type="text/javascript">
+		$(document).ready(function() {
+				$('.mywishlist').on('click', function() {
+						var id = $(this).data('id');
+						//alert(id);
+						if (id) {
+								$.ajax({
+										url: "{{ url('/product/mobile/add/wishlist/') }}/" + id,
+										type: "GET",
+										dataType: "json",
+										processData: false,
+										success: function(data) {
+												console.log(data);
+												if (data.check) {
+														toastr.error("Already This Product Add wishlist");
+												} else {
+														toastr.success("Product Add To wishlist");
+												}
+										}
+								});
+						} else {
+								alert('danger');
+						}
+				});
+
+		});
+</script>
+
+<!-- compare -->
+	<script>
+			$(document).ready(function() {
+					$('.compareproduct').on('click', function() {
+							var id = $(this).data('id');
+							//alert(id);
+							$.ajax({
+									type: 'GET',
+									url: "{{ url('/product/mobile/compare') }}/" + id,
+									processData: false,
+									success: function(data) {
+											if (data.checkip) {
+													toastr.error("Already This Product Add Compare");
+
+											} else {
+													toastr.success("product add to compare");
+
+											}
+									}
+							});
+					});
+			});
+	</script>
 @endforeach
--- resources/views/mobile/search/search_result_page.blade.php
+++ resources/views/mobile/search/search_result_page.blade.php
@@ -1,6 +1,7 @@
 <!-- Begin Bar Nav -->
 @extends('mobile.extra_master')
 @section('page_content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <header class="bar bar-nav ">
 <a class="btn btn-link btn-nav pull-left" href="{{ url('/') }}">
         <span class="icon icon-left-nav"></span>
@@ -89,7 +90,7 @@
                                 <div class="product-item-container">
                                     <div class="left-block">
                                         <div class="product-image-container ">
-                                            <a href="#" title="amper modi dolores">
+                                            <a href="{{ url('product/details/'.$product->slug.'/'.$product->id) }}" title="">
                                                 <img src="{{ asset('public/uploads/products/thumbnail/mobile/'.$product->thumbnail_img) }}"
                                                     title="{{ $product->product_name }}" class="img-responsive">
                                             </a>
@@ -107,7 +108,7 @@
                                     <div class="right-block">
                                         <div class="caption">
                                             <h4><a
-                                                    href="{{ url('product/details/'.$product->slug.'/'.$product->id) }}">{{ $product->product_name }}</a>
+                                                    href="{{ url('product/details/'.$product->slug.'/'.$product->id) }}">{{ Str::limit($product->product_name,10) }}</a>
                                             </h4>
                                             <div class="ratings">
                                                 <div class="rating-box">
@@ -124,24 +125,25 @@
                                                 </div>
                                             </div>
                                             <div class="description ">
-                                                <p>{{ $product->product_description }}</p>
+                                                <p>{!! Str::limit($product->product_description,20) !!}</p>
                                             </div>
                                             <div class="price">
                                                 @if ($productInFlashDeal)
                                                 <span class="price-new">
                                                     @php
-                                                    $discountPrice = 0;
+                                                    $disprice = 0;
                                                     @endphp
                                                     @if($productInFlashDeal->discount_type == 1)
                                                         @php
-                                                        $discountPrice = $product->product_price - $productInFlashDeal->discount;
+                                                        $disprice = $product->product_price - $productInFlashDeal->discount;
                                                         @endphp
-                                                    @else
+                                                    @elseif($productInFlashDeal->discount_type == 2)
                                                         @php
                                                         $discountPrice = $product->product_price /100 * $productInFlashDeal->discount;
+                                                        $disprice= $product->product_price-$discountPrice;
                                                         @endphp
                                                     @endif
-                                                    ৳ {{ $discountPrice }}
+                                                    ৳ {{ $disprice }}
                                                 </span>
                                                 <span class="price-old">
                                                     ৳ {{ $product->product_price }}
@@ -153,15 +155,13 @@
                                                 @endif
                                             </div>
                                             <div class="button-group">
-                                                <button class="addToCart font-sn" type="button" title="Add to Cart"
-                                                    onclick="cart.add('175', '1');"> <i
-                                                        class="fa fa-shopping-cart"></i><span><span>Add to
-                                                            Cart</span></span></button>
-                                                <button class="wishlist btn-button" type="button" title="Add to Wish List"
-                                                    onclick="wishlist.add('175');"><i class="fa fa-heart-o"></i></button>
-                                                <button class="compare btn-button" type="button"
-                                                    title="Compare this Product" onclick="compare.add('175');"><i
-                                                        class="fa fa-retweet"></i></button>
+                                                <button class="addToCart font-sn" type="button" title="Add to Cart" onclick="cart.add('175', '1');"> <i class="fa fa-shopping-cart"></i><span><span>Add to Cart</span></span></button>
+                                                @if(Auth::guard('web')->check())
+                                                <a class="wishlist btn-button mywishlist" id="mywishlist" data-id="{{$product->id}}"><i class="fa fa-heart-o"></i></a>
+                                                @else
+                                                  <a href="{{url('/mobile/login')}}" class="wishlist btn-button"  title="Add to Wish List" href="" ><i class="fa fa-heart-o"></i></a>
+                                                @endif
+                                                <a class="compare btn-button compareproduct" type="button"  title="Compare this Product" id="compareproduct" data-id="{{$product->id}}"><i class="fa fa-retweet"></i></a>
                                             </div>
                                         </div>
                                     </div>
@@ -185,5 +185,55 @@
     </div>
     <!-- //End Main Content -->
 </div>
+<script type="text/javascript">
+		$(document).ready(function() {
+				$('.mywishlist').on('click', function() {
+						var id = $(this).data('id');
+						//alert(id);
+						if (id) {
+								$.ajax({
+										url: "{{ url('/product/mobile/add/wishlist/') }}/" + id,
+										type: "GET",
+										dataType: "json",
+										processData: false,
+										success: function(data) {
+												console.log(data);
+												if (data.check) {
+														toastr.error("Already This Product Add wishlist");
+												} else {
+														toastr.success("Product Add To wishlist");
+												}
+										}
+								});
+						} else {
+								alert('danger');
+						}
+				});
 
+		});
+</script>
+
+<!-- compare -->
+	<script>
+			$(document).ready(function() {
+					$('.compareproduct').on('click', function() {
+							var id = $(this).data('id');
+							//alert(id);
+							$.ajax({
+									type: 'GET',
+									url: "{{ url('/product/mobile/compare') }}/" + id,
+									processData: false,
+									success: function(data) {
+											if (data.checkip) {
+													toastr.error("Already This Product Add Compare");
+
+											} else {
+													toastr.success("product add to compare");
+
+											}
+									}
+							});
+					});
+			});
+	</script>
 @endsection
